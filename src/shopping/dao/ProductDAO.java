ffc83b5ba5be08d490f581803dc8f52470fd4f3e
wujia <wujia@DESKTOP-0R96SGO>
2017-05-10 19:36:01 +0800

    5.10 1

---
+++ src/shopping/dao/ProductDAO.java
@@ -0,0 +1,228 @@
+package shopping.dao;
+
+import java.util.List;
+
+import org.hibernate.LockOptions;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import shopping.model.Product;
+
+/**
+ 	* A data access object (DAO) providing persistence and search support for Product entities.
+ 			* Transaction control of the save(), update() and delete() operations 
+		can directly support Spring container-managed transactions or they can be augmented	to handle user-managed Spring transactions. 
+		Each of these methods provides additional information for how to configure it for the desired type of transaction control. 	
+	 * @see shopping.model.Product
+  * @author MyEclipse Persistence Tools 
+ */
+    @Transactional   
+public class ProductDAO  {
+	     private static final Logger log = LoggerFactory.getLogger(ProductDAO.class);
+		//property constants
+	public static final String PNAME = "pname";
+	public static final String PRICE = "price";
+	public static final String PSTOCK = "pstock";
+	public static final String PTYPE = "ptype";
+	public static final String PMPIC = "pmpic";
+	public static final String PDPIC = "pdpic";
+	public static final String PDETAIL = "pdetail";
+	public static final String PRODUCTCOL = "productcol";
+	public static final String PDESCRIPTION = "pdescription";
+	public static final String PSALES = "psales";
+
+
+
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory){
+       this.sessionFactory = sessionFactory;
+    }
+
+    private Session getCurrentSession(){
+     return sessionFactory.getCurrentSession(); 
+    }
+	protected void initDao() {
+		//do nothing
+	}
+    
+    public void save(Product transientInstance) {
+        log.debug("saving Product instance");
+        try {
+            getCurrentSession().save(transientInstance);
+            log.debug("save successful");
+        } catch (RuntimeException re) {
+            log.error("save failed", re);
+            throw re;
+        }
+    }
+    
+	public void delete(Product persistentInstance) {
+        log.debug("deleting Product instance");
+        try {
+            getCurrentSession().delete(persistentInstance);
+            log.debug("delete successful");
+        } catch (RuntimeException re) {
+            log.error("delete failed", re);
+            throw re;
+        }
+    }
+    
+    public Product findById( java.lang.String id) {
+        log.debug("getting Product instance with id: " + id);
+        try {
+            Product instance = (Product) getCurrentSession()
+                    .get("shopping.model.Product", id);
+            return instance;
+        } catch (RuntimeException re) {
+            log.error("get failed", re);
+            throw re;
+        }
+    }
+    
+    
+    public List findByExample(Product instance) {
+        log.debug("finding Product instance by example");
+        try {
+            List results = getCurrentSession().createCriteria("shopping.model.Product") .add(Example.create(instance)).list();
+            log.debug("find by example successful, result size: " + results.size());
+            return results;
+        } catch (RuntimeException re) {
+            log.error("find by example failed", re);
+            throw re;
+        }
+    }    
+    
+    public List findByProperty(String propertyName, Object value) {
+      log.debug("finding Product instance with property: " + propertyName
+            + ", value: " + value);
+      try {
+         String queryString = "from Product as model where model." 
+         						+ propertyName + "= ?";
+         Query queryObject = getCurrentSession().createQuery(queryString);
+		 queryObject.setParameter(0, value);
+		 return queryObject.list();
+      } catch (RuntimeException re) {
+         log.error("find by property name failed", re);
+         throw re;
+      }
+	}
+
+	public List findByPname(Object pname
+	) {
+		return findByProperty(PNAME, pname
+		);
+	}
+	
+	public List findByPrice(Object price
+	) {
+		return findByProperty(PRICE, price
+		);
+	}
+	
+	public List findByPstock(Object pstock
+	) {
+		return findByProperty(PSTOCK, pstock
+		);
+	}
+	
+	public List findByPtype(Object ptype
+	) {
+		return findByProperty(PTYPE, ptype
+		);
+	}
+	
+	public List findByPmpic(Object pmpic
+	) {
+		return findByProperty(PMPIC, pmpic
+		);
+	}
+	
+	public List findByPdpic(Object pdpic
+	) {
+		return findByProperty(PDPIC, pdpic
+		);
+	}
+	
+	public List findByPdetail(Object pdetail
+	) {
+		return findByProperty(PDETAIL, pdetail
+		);
+	}
+	
+	public List findByProductcol(Object productcol
+	) {
+		return findByProperty(PRODUCTCOL, productcol
+		);
+	}
+	
+	public List findByPdescription(Object pdescription
+	) {
+		return findByProperty(PDESCRIPTION, pdescription
+		);
+	}
+	
+	public List findByPsales(Object psales
+	) {
+		return findByProperty(PSALES, psales
+		);
+	}
+	
+
+	public List findAll() {
+		log.debug("finding all Product instances");
+		try {
+			String queryString = "from Product";
+	         Query queryObject = getCurrentSession().createQuery(queryString);
+			 return queryObject.list();
+		} catch (RuntimeException re) {
+			log.error("find all failed", re);
+			throw re;
+		}
+	}
+	
+    public Product merge(Product detachedInstance) {
+        log.debug("merging Product instance");
+        try {
+            Product result = (Product) getCurrentSession()
+                    .merge(detachedInstance);
+            log.debug("merge successful");
+            return result;
+        } catch (RuntimeException re) {
+            log.error("merge failed", re);
+            throw re;
+        }
+    }
+
+    public void attachDirty(Product instance) {
+        log.debug("attaching dirty Product instance");
+        try {
+            getCurrentSession().saveOrUpdate(instance);
+            log.debug("attach successful");
+        } catch (RuntimeException re) {
+            log.error("attach failed", re);
+            throw re;
+        }
+    }
+    
+    public void attachClean(Product instance) {
+        log.debug("attaching clean Product instance");
+        try {
+                      	getCurrentSession().buildLockRequest(LockOptions.NONE).lock(instance);
+          	            log.debug("attach successful");
+        } catch (RuntimeException re) {
+            log.error("attach failed", re);
+            throw re;
+        }
+    }
+
+	public static ProductDAO getFromApplicationContext(ApplicationContext ctx) {
+    	return (ProductDAO) ctx.getBean("ProductDAO");
+	}
+}
---
+++ src/shopping/model/Product.java
@@ -0,0 +1,152 @@
+package shopping.model;
+
+
+
+/**
+ * Product entity. @author MyEclipse Persistence Tools
+ */
+
+public class Product  implements java.io.Serializable {
+
+
+    // Fields    
+
+     private String pid;
+     private String pname;
+     private Double price;
+     private Double pstock;
+     private String ptype;
+     private String pmpic;
+     private String pdpic;
+     private String pdetail;
+     private String productcol;
+     private String pdescription;
+     private Integer psales;
+
+
+    // Constructors
+
+    /** default constructor */
+    public Product() {
+    }
+
+	/** minimal constructor */
+    public Product(String pid) {
+        this.pid = pid;
+    }
+    
+    /** full constructor */
+    public Product(String pid, String pname, Double price, Double pstock, String ptype, String pmpic, String pdpic, String pdetail, String productcol, String pdescription, Integer psales) {
+        this.pid = pid;
+        this.pname = pname;
+        this.price = price;
+        this.pstock = pstock;
+        this.ptype = ptype;
+        this.pmpic = pmpic;
+        this.pdpic = pdpic;
+        this.pdetail = pdetail;
+        this.productcol = productcol;
+        this.pdescription = pdescription;
+        this.psales = psales;
+    }
+
+   
+    // Property accessors
+
+    public String getPid() {
+        return this.pid;
+    }
+    
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return this.pname;
+    }
+    
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+    
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPstock() {
+        return this.pstock;
+    }
+    
+    public void setPstock(Double pstock) {
+        this.pstock = pstock;
+    }
+
+    public String getPtype() {
+        return this.ptype;
+    }
+    
+    public void setPtype(String ptype) {
+        this.ptype = ptype;
+    }
+
+    public String getPmpic() {
+        return this.pmpic;
+    }
+    
+    public void setPmpic(String pmpic) {
+        this.pmpic = pmpic;
+    }
+
+    public String getPdpic() {
+        return this.pdpic;
+    }
+    
+    public void setPdpic(String pdpic) {
+        this.pdpic = pdpic;
+    }
+
+    public String getPdetail() {
+        return this.pdetail;
+    }
+    
+    public void setPdetail(String pdetail) {
+        this.pdetail = pdetail;
+    }
+
+    public String getProductcol() {
+        return this.productcol;
+    }
+    
+    public void setProductcol(String productcol) {
+        this.productcol = productcol;
+    }
+
+    public String getPdescription() {
+        return this.pdescription;
+    }
+    
+    public void setPdescription(String pdescription) {
+        this.pdescription = pdescription;
+    }
+
+    public Integer getPsales() {
+        return this.psales;
+    }
+    
+    public void setPsales(Integer psales) {
+        this.psales = psales;
+    }
+   
+
+
+
+
+
+
+
+
+}
